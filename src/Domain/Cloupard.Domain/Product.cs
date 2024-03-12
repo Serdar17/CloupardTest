21f1e9402be20865084909c4eb8de7f2e2a5d595
serdar17 <useinovserdar23@gmail.com>
2024-03-12 19:22:08 +0500

    feat(proj): Init project and added main layers

---
+++ src/Domain/Cloupard.Domain/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cloupard.Api;
+
+public partial class Product
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Description { get; set; }
+}
